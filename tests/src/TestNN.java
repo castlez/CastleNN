59a0285bea6839ee8ea8f04528d2c556e54bea4c
castlez <castlez93@gmail.com>
2016-08-15 02:30:13 -0700

    add test folder with junit, update git ignore

---
+++ tests/src/TestNN.java
@@ -0,0 +1,17 @@
+import org.junit.Assert;
+import org.junit.Test;
+
+/**
+ * Created by Jonny on 8/15/2016.
+ */
+
+public class TestNN {
+    @Test
+    public void test_ConstructNN(){
+        try {
+            NeuralNet nn;
+        } catch (Exception e) {
+            throw new AssertionError(e.getMessage());
+        }
+    }
+}
